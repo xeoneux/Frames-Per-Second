8f8eacdec34a0b053b09632f79634d9d92b0ed31
Aayush Kapoor <xeoneux@icloud.com>
2016-10-07 15:32:26 +1100

    Setup detail view controller

--- Frames Per Second/DetailViewController.swift
+++ Frames Per Second/DetailViewController.swift
@@ -13,10 +13,38 @@ class DetailViewController: UIViewController {
     var content: Any!
     var contentType: ContentType?
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var imageView: UIImageView!
+
+    @IBOutlet weak var closeButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        if let contentType = contentType {
+            switch contentType {
+            case .tv:
+                let content = self.content as! Tv
+                label.text = content.title
+                textView.text = content.overview
+                imageView.image = UIImage(data: content.imageData as! Data)
+            case .movie:
+                let content = self.content as! Movie
+                label.text = content.title
+                textView.text = content.overview
+                imageView.image = UIImage(data: content.imageData as! Data)
+            case .person:
+            let content = self.content as! Person
+            label.text = content.name
+            textView.text = ""
+            imageView.image = UIImage(data: content.imageData as! Data)
+            }
+        }
     }
 
+    @IBAction func dismiss(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+    }
 }
 
